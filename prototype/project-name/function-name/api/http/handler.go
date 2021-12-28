34b47a8905cc7472408bd0563aeea44d728ecb3b
Jheison Alejandro Morales Vasquez <jheison.morales@avaldigitallabs.com>
2021-12-28 18:29:56 -0500

    :egg: se agregan carpetas internal/domain y port

--- prototype/project-name/function-name/api/http/handler.go
+++ prototype/project-name/function-name/api/http/handler.go
@@ -1,46 +1,22 @@
 package http
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-
 	"github.com/aws/aws-lambda-go/events"
-)
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
+	"repo-name/project-name/function-name/internal/domain"
 )
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+	// events.APIGatewayProxyRequest -> Objeto de Dominio ->
+	// Invocar al servicio interno de dominio -> events.APIGatewayProxyResponse
+
+	resp, err := domain.GetIp()
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       resp,
 		StatusCode: 200,
 	}, nil
 }
--- prototype/project-name/function-name/cmd/lambda/main.go
+++ prototype/project-name/function-name/cmd/lambda/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/aws/aws-lambda-go/lambda"
 	"repo-name/project-name/function-name/api/http"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	fmt.Print("Hola mundo desde main")
 	lambda.Start(http.Handler)
 }
